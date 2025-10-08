dfe74be5f5a19bd578707f10576228e9efd1f7ae
Haxkx <captainking016@gmail.com>
2025-10-08 21:53:55 +0600

    Update symbols.js

--- api/symbols.js
+++ api/symbols.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   // CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -34,4 +34,4 @@ export default async function handler(req, res) {
     timeframes: timeframes,
     lastUpdate: new Date().toISOString()
   });
-}
+};
